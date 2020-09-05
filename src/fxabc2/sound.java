3af5d4f36ef4400fd23687fe929e4a48dfaaac0a
Kenjiru-san <nene1yuno1kawaii@gmail.com>
2020-09-05 20:59:04 +0900

    [add]add sound.java

--- src/fxabc2/sound.java
+++ src/fxabc2/sound.java
@@ -1,24 +1,61 @@
 package fxabc2;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 
-public class sound {
-	
-	boolean _isSound;
-	String strFile; 
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
-
-	File f = new File(strFile);
-	soundTh th = new soundTh(f);
-}
-
-class soundTh implements Runnable{
-	
-	soundTh(File f){
-	}
-	@Override
+public class sound implements Runnable{
+	String str;
 	public void run() {
-		
+		Sound(str);
 	}
-	
+	public static void Sound(String fileName) {
+		File f = new File(fileName);
+		Clip clip = createClip(f);
+		clip.start();
+		try {
+			Thread.sleep(300);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+        clip.drain();
+        clip.stop();
+	}
+
+	public static Clip createClip(File path) {
+		//指定されたURLのオーディオ入力ストリームを取得
+		try (AudioInputStream ais = AudioSystem.getAudioInputStream(path)){
+		
+			//ファイルの形式取得
+			AudioFormat af = ais.getFormat();
+		
+			//単一のオーディオ形式を含む指定した情報からデータラインの情報オブジェクトを構築
+			DataLine.Info dataLine = new DataLine.Info(Clip.class,af);
+		
+			//指定された Line.Info オブジェクトの記述に一致するラインを取得
+			Clip c = (Clip)AudioSystem.getLine(dataLine);
+		
+			//再生準備完了
+			c.open(ais);
+		
+			return c;
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (UnsupportedAudioFileException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (LineUnavailableException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
